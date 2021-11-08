b920f7303994cfa787108bd786196cc40ccd121d
ManoLo2ManoLo <manuel_canas@aol.com>
2021-11-08 14:22:39 -0500

    fixed error where notes stopped displaying

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,21 +1,31 @@
 const router = require('express').Router();
 const {
+    findById,
     createNotes,
     validateNotes
 } = require('../../lib/notes')
-const { notes } = require('../../data/db');
+const { notesArray } = require('../../data/db');
 
 router.get('/notes', (req, res) => {
-    res.json(notes);
+    res.json(notesArray);
 })
 
+router.get('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notesArray);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
 router.post('/notes', (req, res) => {
-    req.body.id = notes.length.toString();
+    req.body.id = notesArray.length.toString();
 
     if(!validateNotes(req.body)) {
         res.status(400).send('This notes is note not properly formatted.')
     } else {
-        const note = createNotes(req.body, notes)
+        const note = createNotes(req.body, notesArray)
         res.json(note);
     }
 })
